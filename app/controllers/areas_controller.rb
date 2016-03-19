a92bea5318e09ae8c92d30105d30b696d4460c22
harold <tongtakent@gmail.com>
2016-03-19 15:33:56 +0800

    area controller done need modification

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,2 +1,52 @@
 class AreasController < ApplicationController
+
+  def index
+    @area = Area.all
+  end
+
+  def show
+    set_area
+  end
+
+  def new
+  end
+
+  def create
+    @area = Area.new(area_params)
+    if @area.save!
+      redirect_to areas_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    set_area
+  end
+
+  def update
+    set_area
+    if @area.update(area_params)
+      redirect_to areas_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    set_area
+    @area.destroy
+    redirect_to areas_path
+  end
+
+  private
+
+  def area_params
+    params.require(:area).permit(:name, :address, :city, :postcode, :state, :image)
+  end
+
+  def set_area
+    @area = Area.find(params[:id])
+  end
+
 end
--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,6 +1,7 @@
 class HomepagesController < ApplicationController
 
   def index
+    @food = Food.all
   end
 
 end
